5bc8dc91008e7f37629ca3df179133461d2a826a
mohitmaingit <lalwanihome146@gmail.com>
2025-08-21 17:12:57 +0530

    Fix App.tsx syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-@@ .. @@
- import React from 'react';
-+import GameApp from './ui/App';
+import React from 'react';
+import GameApp from './ui/App';
 
- function App() {
-   return (
--    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
--      <p>Start prompting (or editing) to see magic happen :)</p>
--    </div>
-+    <GameApp />
-   );
- }
+function App() {
+  return (
+    <GameApp />
+  );
+}
 
- export default App;
+export default App;
